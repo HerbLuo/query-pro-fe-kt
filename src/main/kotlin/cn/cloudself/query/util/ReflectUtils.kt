26e393f1693c91a2f1898a4db7f01deff1fa2f4e
HerbLuo <cloudself.cn@gmail.com>
2021-12-28 20:34:43 +0800

    canAccess, isAccessible

--- src/main/kotlin/cn/cloudself/query/util/ReflectUtils.kt
+++ src/main/kotlin/cn/cloudself/query/util/ReflectUtils.kt
@@ -2,7 +2,7 @@ package cn.cloudself.query.util
 
 import java.lang.reflect.Field
 
-val areBelowOfVersion8 = Runtime::class.java.`package`.implementationVersion.startsWith("1.")
+val areBelowOfVersion8 = System.getProperty("java.version").startsWith("1.")
 
 fun canAccess(field: Field, o: Any?): Boolean {
     return if (areBelowOfVersion8) {
